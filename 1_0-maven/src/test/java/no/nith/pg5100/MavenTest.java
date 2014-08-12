4f49bd8c5fff5ef592bb77fa89a69d33bb51a443
Jarle Hansen <jarle@jarlehansen.net>
2014-08-12 13:43:23 +0200

    initial commit

---
+++ 1_0-maven/src/test/java/no/nith/pg5100/MavenTest.java
@@ -0,0 +1,14 @@
+package no.nith.pg5100;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MavenTest {
+
+    @Test
+    public void test() throws Exception {
+        String s = "";
+        assertEquals(0, s.length());
+    }
+}
