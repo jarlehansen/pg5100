fc80a00f1c7126a5fea1bebe2657c0e8e8fb3ccd
Jarle Hansen <jarle@jarlehansen.net>
2014-08-16 16:13:12 +0200

    added 1_1-h2-demo

---
+++ 1_1-h2-demo/src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        try (Connection connection = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/tools/server/db-files/h2-demo", "sa", "sa")) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES");
+
+            System.out.println("Default tables");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1));
+            }
+        }
+    }
+}
