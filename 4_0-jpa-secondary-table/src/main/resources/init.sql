78a67171b6256b376729c3768439ec5002f03327
Jarle Hansen <jarle@jarlehansen.net>
2014-09-20 12:51:24 +0200

    4_0 jpa secondary table

---
+++ 4_0-jpa-secondary-table/src/main/resources/init.sql
@@ -0,0 +1,4 @@
+INSERT INTO person (id, name, age) VALUES (1, 'Ola Nordmann', 25);
+INSERT INTO person (id, name, age) VALUES (2, 'Kari Olsen', 74);
+INSERT INTO address (id, street, zip) values(1, 'Testveien 1', '0185')
+INSERT INTO address (id, street, zip) values(2, 'Testveien 2', '2000')
