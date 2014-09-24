66c44a928b10e605c462f0917a8b5271619b6e1c
Jarle Hansen <jarle@jarlehansen.net>
2014-09-24 19:13:36 +0200

    jpa relationship mapping

--- 4_5-jpa-relationship-mapping/src/main/java/no/nith/pg5100/Main.java
+++ 4_5-jpa-relationship-mapping/src/main/java/no/nith/pg5100/Main.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("person");
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("relationship-mapping");
         EntityManager entityManager = factory.createEntityManager();
 
         // One to one
