3e0e3287c776b138b399dba5c9f2cad10a0c986e
Jarle Hansen <jarle@jarlehansen.net>
2014-09-28 13:23:57 +0200

    jsf intro

--- 5_0-jsf-intro/src/main/java/no/nith/pg5100/PersonController.java
+++ 5_0-jsf-intro/src/main/java/no/nith/pg5100/PersonController.java
@@ -2,11 +2,10 @@ package no.nith.pg5100;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
-import java.io.Serializable;
 
 @Named
 @RequestScoped
-public class PersonController implements Serializable {
+public class PersonController {
 
     // http://localhost:8080/5_0-jsf-intro-1.0-SNAPSHOT/
     public Person getPerson() {
