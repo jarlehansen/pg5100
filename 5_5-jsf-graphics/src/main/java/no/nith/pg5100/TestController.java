64ec8ec4377c625df67550340a82094d13d7d5ff
Jarle Hansen <jarle@jarlehansen.net>
2014-10-02 20:17:27 +0200

    jsf eksempler

--- 5_5-jsf-graphics/src/main/java/no/nith/pg5100/TestController.java
+++ 5_5-jsf-graphics/src/main/java/no/nith/pg5100/TestController.java
@@ -5,6 +5,7 @@ import javax.enterprise.inject.Model;
 @Model
 public class TestController {
 
+    // http://localhost:8080/5_5-jsf-graphics-1.0-SNAPSHOT/index.jsf
     public String getImage() {
         return "westerdals-oslo-act.png";
     }
