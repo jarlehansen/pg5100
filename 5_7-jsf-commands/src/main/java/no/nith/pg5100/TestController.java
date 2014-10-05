be910ca9a3071d8405403c6f5614e6c930249813
Jarle Hansen <jarle@jarlehansen.net>
2014-10-05 14:47:44 +0200

    jsf

--- 5_7-jsf-commands/src/main/java/no/nith/pg5100/TestController.java
+++ 5_7-jsf-commands/src/main/java/no/nith/pg5100/TestController.java
@@ -5,7 +5,7 @@ import javax.enterprise.inject.Model;
 @Model
 public class TestController {
 
-    // http://localhost:8080/5_7-jsf-commands-1.0-SNAPSHOT/index.faces
+    // http://localhost:8080/5_7-jsf-commands-1.0-SNAPSHOT/index.jsf
     public void onSubmit() {
         System.out.println("Submit button pressed");
     }
