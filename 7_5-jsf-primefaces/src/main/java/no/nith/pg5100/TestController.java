e92545a1a7a9a8cae8a3a392a023ad1258c280a5
Jarle Hansen <jarle@jarlehansen.net>
2014-10-28 22:06:21 +0100

    primefaces

--- 7_5-jsf-primefaces/src/main/java/no/nith/pg5100/TestController.java
+++ 7_5-jsf-primefaces/src/main/java/no/nith/pg5100/TestController.java
@@ -4,9 +4,12 @@ import javax.enterprise.inject.Model;
 import java.util.ArrayList;
 import java.util.List;
 
+// http://localhost:8080/7_5-jsf-primefaces-1.0-SNAPSHOT/index.jsf
 @Model
 public class TestController {
     private String text;
+    private String value1;
+    private String value2;
 
     public String getText() {
         return text;
@@ -16,6 +19,22 @@ public class TestController {
         this.text = text;
     }
 
+    public String getValue1() {
+        return value1;
+    }
+
+    public void setValue1(String value1) {
+        this.value1 = value1;
+    }
+
+    public String getValue2() {
+        return value2;
+    }
+
+    public void setValue2(String value2) {
+        this.value2 = value2;
+    }
+
     public List<String> complete(String query) {
         List<String> autocomplete = new ArrayList<>();
         for (int i = 1; i <= 10; i++) {
@@ -23,5 +42,11 @@ public class TestController {
         }
 
         return autocomplete;
+    }
+
+    public void submit() {
+        System.out.println("Value1: " + value1);
+        System.out.println("Value2: " + value2);
+        System.out.println("TestController.submit()");
     }
 }
