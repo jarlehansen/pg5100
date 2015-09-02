42f882152938dc65219c36f36bb07755acfa426e
Jarle Hansen <jarle@jarlehansen.net>
2015-09-02 21:48:53 +0200

    wildfly 9.0.1 + java 1.8

--- 1_2-h2-demo/start-db.sh
+++ 1_2-h2-demo/start-db.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # jdbc:h2:tcp://localhost/~/tools/server/db-files/h2-demo sa/sa
-java -cp ~/tools/server/wildfly-8.1.0.Final/modules/system/layers/base/com/h2database/h2/main/h2-1.3.173.jar org.h2.tools.Server
+java -cp ~/tools/server/wildfly-9.0.1.Final/modules/system/layers/base/com/h2database/h2/main/h2-1.3.173.jar org.h2.tools.Server
